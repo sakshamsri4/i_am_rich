5ef6812f5aaf963d802e8cfa374666c735b4271e
sakshamsri4 <sakshamsri4@gmail.com>
2020-06-02 14:15:41 +0530

    first commit

---
+++ android/app/src/main/java/com/stringlabs/iamrich/MainActivity.java
@@ -0,0 +1,6 @@
+package com.stringlabs.iamrich;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey[900],
+        title: Text('I am Rich'),
+      ),
+      body: Center(
+        child: Image(image: AssetImage('images/download.jpg')),
+      ),
+    ),
+  ));
+}
